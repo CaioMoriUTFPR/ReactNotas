740947ec2f9fd9fb926a0ebdb3585be77e0069b4
Caio Mori <caiomori@alunos.utfpr.edu.br>
2021-08-04 08:47:40 -0300

    React padrao

---
+++ ceep/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <form>
+      <input type="text" placeholder="Título"/>
+      <textarea placeholder="Escreva sua nota"></textarea>
+      <button>Criar Nota</button>
+    </form>
+  );
+}
+
+export default App;
